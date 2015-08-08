221dad505386d22fa9c37478df07945afc10eb1d
nickgoodrow <nickgoodrow@gmail.com>
2015-08-08 12:56:19 -0400

    Add Basic login forms and validation and Model

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -11,8 +12,39 @@ namespace SimpleBlog.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return View();
-        }       
+            return View(new AuthLogin()
+            {
+                Test = "Test value obj instanciate with default GET",
+                Valid = "",
+                Username = "Default"
+            });
+        }
+
+        // Post here
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            // we use the form to change data and pass it to next view
+            form.Test = "Value of Test changes after POST ";
+            form.Valid = "The form info is valid";
+
+            // check for invalid input
+            if (!ModelState.IsValid)
+            {
+                form.Valid = "! - The form is INVALID";
+                return View(form);
+            }
+
+            // check is user/email valid loop back from db per say
+            if (form.Username != "admin")
+            {
+                ModelState.AddModelError("Username", "USernamer or password was not found!");
+                form.Valid = "! - The form is INVALID";
+                return View(form);
+            }
+
+            return View(form);
+        }
     }
 
     
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Test { get; set; }
+
+        public string Valid { get; set; }
+    }
+}
