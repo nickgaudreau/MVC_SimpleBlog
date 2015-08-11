f7fd4bd32aab332c63d637190a8e35dd3be15c5a
nickgoodrow <nickgoodrow@gmail.com>
2015-08-10 20:19:20 -0400

    Imported styles and scripts and set bundle config in APp_start and global.asax

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Style Admin
+            // how we will target them from Views : new StyleBundle("~/admin/styles")
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/admin.css"));
+
+            // Style Main
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+
+            // Script -> Order is important!!
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/Scripts/jquery-2.1.4.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/SimpleBlog/Scripts/bootstrap.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/Scripts/jquery-2.1.4.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/SimpleBlog/Scripts/bootstrap.js")
+                );
+
+        }
+    }
+}
